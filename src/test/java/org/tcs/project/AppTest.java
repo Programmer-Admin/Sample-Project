36cb66a314d2e3f990833fff5df3ffc74990e724
ABHIK <abkbsws1996@gmail.com>
2022-06-23 21:46:53 +0530

    initial Commit

---
+++ src/test/java/org/tcs/project/AppTest.java
@@ -0,0 +1,21 @@
+package org.tcs.project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println(System.getProperty("name"));
+        assertTrue( true );
+    }
+}
